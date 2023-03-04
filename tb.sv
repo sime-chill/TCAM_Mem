fa3587f73c278888b2a9c964a87c72bd4898b249
yuhan <heyh39@mail2.sysu.edu.cn>
2023-03-04 21:07:30 +0800

    2rst

--- tb.sv
+++ tb.sv
@@ -264,10 +264,10 @@ module tb
     );
     //to reset successfully, must initialize other mode signals
     PacketID_In <= ZERO_ID;
-    rst         <= 1;
+    rst         = 1;
     MODE        <= MODE_RST;
-    #PERIOD;
-    rst         <= 0;
+    #(PERIOD * 2);
+    rst         = 0;
     MODE        <= MODE_I;
     #PERIOD;
   endtask
